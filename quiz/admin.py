c7ccabeca184081e4e5d61b378310fb0d9bd5d78
ecuatox <a.tenstad@gmail.com>
2017-05-06 22:20:03 +0200

    Create quiz models

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Section, Subsection, Question, Answer
+
+admin.site.register(Section)
+admin.site.register(Subsection)
+admin.site.register(Question)
+admin.site.register(Answer)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Section(models.Model):
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.number
+
+
+class Subsection(models.Model):
+    section = models.ForeignKey(Section)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return '%r.%r' % (self.section, self.number)
+
+
+class Question(models.Model):
+    subsection = models.ForeignKey(Subsection)
+    number = models.IntegerField()
+    question = models.CharField(max_length=300)
+    true = models.BooleanField(verbose_name='Answer')
+
+    def __str__(self):
+        return self.question if len(self.question) <= 30 else '%s...' % self.question[27:]
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User)
+    question = models.ForeignKey(Question)
+    timestamp = models.DateTimeField(default=timezone.now)
+    correct = models.BooleanField()
