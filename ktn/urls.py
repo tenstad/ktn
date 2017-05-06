c4c56ec018f56067cddad9b0c6e3d3cb99a3d0b9
ecuatox <a.tenstad@gmail.com>
2017-05-06 21:59:33 +0200

    Create quiz app

--- ktn/urls.py
+++ ktn/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', Index.as_view()),
     url(r'^account/', include('account.urls')),
+    url(r'^quiz/', include('quiz.urls')),
 ]
